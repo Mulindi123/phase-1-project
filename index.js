c75292d4dba408cc9a6f83312e3a5a80cb86217f
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-02 14:45:10 +0300

    Implemented the PUT request

--- index.js
+++ index.js
@@ -46,3 +46,77 @@ viewScheduleButton.textContent = "View Schedule"
 viewScheduleButton.addEventListener("click", viewScheduleOnClick)
 sideBar.appendChild(viewScheduleButton)
 sideBar.appendChild(scheduleContainer)
+
+
+const showResourcesButton = document.getElementById("showResources");
+const resourcesContent = document.getElementById("resourcesContent");
+
+showResourcesButton.addEventListener("click", () => {
+    if (resourcesContent.style.display === "none") {
+        resourcesContent.style.display = "block";
+    } else {
+        resourcesContent.style.display = "none";
+    }
+});
+
+
+//Form for users to make garbage collection requests
+const garbageRequestForm = document.querySelector("#garbageRequestForm");
+
+// Function to check if the given day is Friday(I only do special requests on fridays)
+function isFriday(day) {
+    return day.toLowerCase() === "friday";
+}
+
+// Function to check if the given time slot is free on Friday
+function isTimeSlotFree (schedule, timeSlot) {
+    const fridaySchedule = schedule.find(item => isFriday(item.day));
+    return fridaySchedule[timeSlot] === "free";
+}
+
+// Function to update the schedule
+function updateSchedule (day,timeSlot, area) {
+    fetch(dataUrl)
+    .then(res =>res.json())
+    .then(data =>{
+        const fridaySchedule = data.find(item =>isFriday(item.day));
+        fridaySchedule[timeSlot] =area;
+        console.log(timeSlot)
+
+         // Update the schedule on the server
+         fetch(`${dataUrl}/5`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(fridaySchedule)
+         })
+         .then(()=>{
+            alert("Your garbage collection has been submitted for Friday")
+         })
+         .catch(error => console.log(error));
+    })
+}
+garbageRequestForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const area = document.getElementById("area").value
+    const day = document.getElementById("day").value
+    const timeSlot = document.getElementById("timeSlot").value
+
+    if (!isFriday(day)) {
+        alert("Sorry, we only offer garbage collection on Fridays.");
+        return;
+    }
+
+    fetch(dataUrl)
+    .then(res => res.json())
+        .then(data => {
+            if (!isTimeSlotFree(data, timeSlot)) {
+                alert(`Sorry, the ${timeSlot} time slot on Friday is already booked.`);
+            } else {
+                updateSchedule(day, timeSlot, area);
+            }
+        })
+        .catch(error => console.log(error));
+})
